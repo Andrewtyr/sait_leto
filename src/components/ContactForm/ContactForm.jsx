c64405548ec3c081479304d8f39e49bc17fda45b
Andrewtyr <hurohin71@gmail.com>
2024-02-24 17:07:26 +0700

    Contact

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './ContactForm.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVk, faTelegram } from '@fortawesome/free-brands-svg-icons';
+
+function ContactForm() {
+  return (
+    <div className="contact-form-container">
+      {/* Контейнер для текста и иконок */}
+      <div className="container">
+        <table style={{ width: '100%', textAlign: 'center', marginBottom: '50px' }}>
+          <tbody>
+            <tr>
+              <td style={{ textAlign: 'left' }}>
+                <p className="left-align-text">Связаться со мной можно по почте hurohin72@gmail.com<br/>
+                Альтерантива связи может быть мой телеграм  </p>
+              </td>
+              <td>
+                <div className="icons" style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
+                  <a href="ссылка_на_вконтакте" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faVk} className="vk-icon" style={{ fontSize: '50px' }} />
+                  </a>
+                  <a href="ссылка_на_телеграм" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faTelegram} className="telegram-icon" style={{ fontSize: '50px' }} />
+                  </a>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      {/* Контейнер для фото */}
+      <div className="container">
+        <p style={{ textAlign: 'center' }}>
+          <img src={process.env.PUBLIC_URL + '/images/контакты.png'} alt="Фото" />
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ContactForm;
--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,16 +1,46 @@
-// ContactsPage.jsx
+// pages/ContactsPage/ContactsPage.jsx
 import React from 'react';
 import MyNavbar from '../../components/Navbar/Navbar';
-
-
+import ContactForm from '../../components/ContactForm/ContactForm';
+import InfoBlock from '../../components/InfoBlock/InfoBlock';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './ContactsPage.scss';
 
 function ContactsPage() {
   return (
-    <div>
-      <MyNavbar />
-      <h2>Страница контактов</h2>
-      {/* Добавьте контент страницы контактов */}
-    </div>
+    <Container fluid className="main-container">
+      {/* Навигация */}
+      <Row>
+        <Col className="border-0">
+          <MyNavbar />
+        </Col>
+      </Row>
+
+      {/* Увеличенный отступ между навигацией и формой контактов */}
+      <Row className="mt-3">
+        <Col className="bg-custom-gray d-flex justify-content-center align-items-center border-0 custom-contact-form-container" style={{ paddingLeft: '0px', paddingRight: '0px', margin: '0px !important' }}>
+          <ContactForm />
+        </Col>
+      </Row>
+      {/* Последний контейнер для мобильной версии */}
+      <Row className="d-md-none mt-4">
+        <Col className="border-0" style={{ marginTop: '250px' }}>
+          {/* Ваш последний контейнер для мобильной версии */}
+        </Col>
+      </Row>
+      {/* Контейнер для InfoBlock */}
+      <Row className="mt-4">
+        <Col className="border-0">
+          <InfoBlock />
+        </Col>
+      </Row>
+
+      
+
+      {/* Добавьте другие компоненты и контент, если необходимо */}
+    </Container>
   );
 }
 
