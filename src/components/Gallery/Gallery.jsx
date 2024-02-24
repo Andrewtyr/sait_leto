30918042cf62b36c59e3d197793d77e3a99ee7fe
Andrewtyr <hurohin71@gmail.com>
2024-02-24 15:20:36 +0700

    gallery

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,48 @@
+// components/Gallery/Gallery.jsx
+import React from 'react';
+import './Gallery.scss';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
+function Gallery() {
+  return (
+    <div className="gallery-container">
+      {/* Текстовый контейнер сверху */}
+      <div className="text-container">
+        <p className="text">То что мне нравиться</p>
+      </div>
+
+      {/* Контейнер для фотографий */}
+      <Row className="photos-container">
+        {/* Левый столбец */}
+        <Col xs={6} md={6} lg={6} style={{  marginBottom: '20px' }}>
+          <div className="photo-container" style={{  marginBottom: '20px' }}>
+            <Image src={process.env.PUBLIC_URL + '/images/галлерея1.jpg'} alt="Photo 1" className="gallery-image" fluid />
+          </div>
+          <div className="photo-container" style={{  marginBottom: '20px' }}>
+            <Image src={process.env.PUBLIC_URL + '/images/галлерея2.jpg'} alt="Photo 2" className="gallery-image" fluid />
+          </div>
+          <div className="photo-container" style={{  marginBottom: '20px' }}>
+            <Image src={process.env.PUBLIC_URL + '/images/галлерея3.jpg'} alt="Photo 3" className="gallery-image" fluid />
+          </div>
+        </Col>
+
+        {/* Правый столбец */}
+        <Col xs={6} md={6} lg={6}>
+          <div className="photo-container" style={{  marginBottom: '20px' }}>
+            <Image src={process.env.PUBLIC_URL + '/images/галлерея1.jpg'} alt="Photo 4" className="gallery-image" fluid />
+          </div>
+          <div className="photo-container" style={{  marginBottom: '20px' }}>
+            <Image src={process.env.PUBLIC_URL + '/images/галлерея2.jpg'} alt="Photo 5" className="gallery-image" fluid />
+          </div>
+          <div className="photo-container" style={{  marginBottom: '20px' }}>
+            <Image src={process.env.PUBLIC_URL + '/images/галлерея3.jpg'} alt="Photo 6" className="gallery-image" fluid />
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default Gallery;
---
+++ src/pages/GalleryPage/GalleryPage.jsx
@@ -0,0 +1,40 @@
+// pages/GalleryPage/GalleryPage.jsx
+import React from 'react';
+import MyNavbar from '../../components/Navbar/Navbar';
+import Gallery from '../../components/Gallery/Gallery';
+import Container from 'react-bootstrap/Container';
+import InfoBlock from '../../components/InfoBlock/InfoBlock';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './GalleryPage.scss';
+
+function GalleryPage() {
+  return (
+    <Container fluid className="main-container">
+      {/* Навигация */}
+      <Row>
+        <Col>
+          <MyNavbar />
+        </Col>
+      </Row>
+
+      {/* Увеличенный отступ между навигацией и галереей */}
+      <Row className="mt-3">
+        <Col className="d-flex justify-content-center align-items-center">
+          <Gallery />
+        </Col>
+      </Row>
+
+      {/* Контейнер для InfoBlock */}
+      <Row className="mt-4">
+        <Col>
+          <InfoBlock />
+        </Col>
+      </Row>
+
+      {/* Добавьте другие компоненты и контент, если необходимо */}
+    </Container>
+  );
+}
+
+export default GalleryPage;
