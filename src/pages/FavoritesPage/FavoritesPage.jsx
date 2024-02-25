5276b2af9164a5d3443d5a9c70d5cbd5a288e378
Andrewtyr <hurohin71@gmail.com>
2024-02-25 15:30:02 +0700

    final v1.0.0

---
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -0,0 +1,39 @@
+// pages/Favorites/Favorites.jsx
+import React from 'react';
+import MyNavbar from '../../components/Navbar/Navbar';
+import InfoBlock from '../../components/InfoBlock/InfoBlock';
+import Favorites from '../../components/Favorites/Favorites'; // Импортируйте компонент Favorites
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './FavoritesPage.scss'; // Подключите ваши стили
+
+function FavoritesPage() {
+  return (
+    <Container fluid className="main-container">
+      {/* Навигация */}
+      <Row>
+        <Col className="border-0">
+          <MyNavbar />
+        </Col>
+      </Row>
+
+      {/* Увеличенный отступ между навигацией и компонентом Favorites */}
+      <Row className="mt-3">
+        <Col className="custom-favorites-container" style={{ marginBottom: '50px' }}>
+          <Favorites />
+        </Col>
+      </Row>
+
+        {/* Контейнер для InfoBlock */}
+      <Row className="mt-4">
+        <Col className="border-0">
+          <InfoBlock />
+        </Col>
+      </Row>
+      {/* Добавьте другие компоненты и контент, если необходимо */}
+    </Container>
+  );
+}
+
+export default FavoritesPage;
